f518a0ad931111b795f02e548c925caf04ef94e7
FernandaN <fernanda.n1701@hotmail.com>
2021-10-10 20:53:26 -0300

    first commit

---
+++ js/owl/setup.js
@@ -0,0 +1,24 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function assistirAgora(){
+    window.open("https://www.netflix.com/br/title/80204927");
+}
+
+function maisInformacoes(){
+    window.open("https://www.instagram.com/justdessertsau/?hl=pt");
+}
